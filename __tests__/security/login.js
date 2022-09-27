d40cc6c8b6654abff7dab27149a5c042fe5ef375
Mbechezi Mlanawo <mlanawo.mbechezi@alpacode.io>
2022-09-27 13:26:24 +0200

    Fix http code for bad identifiers

--- __tests__/security/login.js
+++ __tests__/security/login.js
@@ -14,7 +14,7 @@ it('Login failed', () => {
     return frisby.post(process.env.API_URL + '/auth/login', {
         email: "toto@toto",
         password: "sdsd"
-    }).expect('status', 400)
+    }).expect('status', 401)
 })
 
 it('login with empty body', () => {
