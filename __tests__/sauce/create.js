c606f1cfb4b5417043e908cd30e961b9b2949324
Mbechezi Mlanawo <mlanawo.mbechezi@alpacode.io>
2022-09-28 20:19:14 +0200

    Clean create sauce

--- __tests__/sauce/create.js
+++ __tests__/sauce/create.js
@@ -4,16 +4,9 @@ const fs = require('fs');
 const login = require('../../hooks/login');
 require('dotenv').config();
 
-it('Create sauce with user token', async () => {
-    const imagePath = path.resolve(__dirname, './../../fixtures/files/18424726.jpg');
-
+it('Create sauce without user token', async () => {
     let formData = frisby.formData();
-    formData.append('imageUrl', imagePath)
-    formData.append('name', 'sdsd');
-    formData.append('manufacturer', 'sqdsqdsq');
-    formData.append('description', 'qss');
-    formData.append('mainPepper', 'qsdsqd');
-    formData.append('heat',  3);
+    formData.append('sauce', JSON.stringify({}))
 
     frisby.fetch(process.env.API_URL + '/sauces', {
         method: 'POST',
@@ -22,19 +15,17 @@ it('Create sauce with user token', async () => {
     }).expect('status', 401)
 })
 
-it('Create sauce with empty body', async () => {
-    await login('toto1@yopmail.com', 'sdsd')
-        .then(data => {
-            let formData = frisby.formData();
+it.only('Create sauce with empty body', async () => {
+    const data = await login('toto1@yopmail.com', 'sdsd')
+    let formData = frisby.formData();
 
-            frisby.fetch(process.env.API_URL + '/sauces', {
-                method: 'POST',
-                'Authorization': "Bearer: " + data.json.token,
-                'Content-type': "application/x-www-form-urlencoded",
-                body: formData
-            })
-                .expect('status', 400)
-        })
+    return frisby.fetch(process.env.API_URL + '/sauces', {
+        method: 'POST',
+        'Authorization': "Bearer: " + data.json.token,
+        'Content-type': "application/x-www-form-urlencoded",
+        body: formData
+    })
+        .expect('status', 400)
 })
 
 it('Create sauce', async () => {
@@ -53,7 +44,7 @@ it('Create sauce', async () => {
     formData.append('image', fs.createReadStream(imagePath));
     formData.append('sauce', JSON.stringify(sauce))
 
-    await frisby.fetch(process.env.API_URL + '/sauces', {
+    return frisby.fetch(process.env.API_URL + '/sauces', {
         method: 'POST',
         headers: {
             'Authorization': "Bearer: " + data.json.token,
