42179a015ac472a6cc037488ed500b6ff31a3d67
Mbechezi Mlanawo <mlanawo.mbechezi@alpacode.io>
2022-09-28 19:10:28 +0200

    Fix create sauce

--- __tests__/sauce/create.js
+++ __tests__/sauce/create.js
@@ -37,25 +37,27 @@ it('Create sauce with empty body', async () => {
         })
 })
 
-it.only('Create sauce', async () => {
-    login('toto1@yopmail.com', 'sdsd')
-        .then(data => {
+it('Create sauce', async () => {
+    const data = await login('toto1@yopmail.com', 'sdsd')
+    const imagePath = path.resolve(__dirname, './../../fixtures/files/18424726.jpg');
 
-            const imagePath = path.resolve(__dirname, './../../fixtures/files/18424726.jpg');
+    let formData = frisby.formData();
+    let sauce  = {
+        name: 'Sauce kipik',
+        manufacturer: 'sqdsq',
+        description: 'qss',
+        mainPepper: 'qsdsqd',
+        heat:  3
+    };
 
-            let formData = frisby.formData();
-            formData.append('imageUrl', imagePath)
-            formData.append('name', 'sdsd');
-            formData.append('manufacturer', 'sqdsqdsq');
-            formData.append('description', 'qss');
-            formData.append('mainPepper', 'qsdsqd');
-            formData.append('heat',  3);
+    formData.append('image', fs.createReadStream(imagePath));
+    formData.append('sauce', JSON.stringify(sauce))
 
-            frisby.fetch(process.env.API_URL + '/sauces', {
-                method: 'POST',
-                'Authorization': "Bearer: " + data.json.token,
-                'Content-type': "application/x-www-form-urlencoded",
-                body: formData
-            }).expect('status', 201)
-        })
+    await frisby.fetch(process.env.API_URL + '/sauces', {
+        method: 'POST',
+        headers: {
+            'Authorization': "Bearer: " + data.json.token,
+        },
+        body: formData
+    }).expect('status', 201)
 })
